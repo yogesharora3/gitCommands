2729d72e33c2deb4babe9d63c84804dd650067a6
yogesharora3 <yogesharora375@gmail.com>
2020-06-30 12:56:34 +0530

    learning github commands

---
+++ a1.js
@@ -0,0 +1,16 @@
+var firstname = "yogesh arora";
+console.log(firstname);
+var age = 28;
+console.log(age);
+var more_than20 = false;
+console.log(more_than20);
+var k;
+console.log(k);
+var firstName, Lastname, age, isMarried;
+firstName = prompt("enter the first name");
+Lastname = prompt("enter the last name");
+age = prompt("enter teh age ");
+isMarried = "false";
+alert(firstName + '   ' + Lastname + '   ' + age + '   ' + isMarried);
+var n = "yogesh"
+console.log(n.slice(-4));
